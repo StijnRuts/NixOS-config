668eb8b95a07d998e2d468c9ffe7fe61c9cc2d7a
Stijn Ruts <git@stijnruts.be>
2024-09-25 12:56:38 +0200

    Modularize

---
+++ system/desktop.nix
@@ -0,0 +1,33 @@
+{ config, pkgs, ... }:
+{
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+
+  services.printing.enable = true;
+
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    # jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    # media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  programs.firefox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    # vim
+    # wget
+  ];
+}
