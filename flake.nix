91ac559c4736f7d000794330a33bb4042163a558
Stijn Ruts <git@stijnruts.be>
2024-10-13 11:12:29 +0000

    Disko

--- flake.nix
+++ flake.nix
@@ -1,16 +1,25 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    disko.url = "github:nix-community/disko/latest";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
-    { self, nixpkgs, ... }@args:
+    {
+      self,
+      nixpkgs,
+      disko,
+      ...
+    }@args:
     {
       nixosConfigurations = {
         X201 = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = args;
           modules = [
-            ./hardware-configuration.nix
+            disko.nixosModules.disko
+            ./disko/X201.nix
+            ./hardware/X201.nix
             ./configuration.nix
           ];
         };
@@ -18,7 +27,9 @@
           system = "x86_64-linux";
           specialArgs = args;
           modules = [
-            ./hardware-configuration.nix
+            disko.nixosModules.disko
+            ./disko/T420.nix
+            ./hardware/T420.nix
             ./configuration.nix
           ];
         };
