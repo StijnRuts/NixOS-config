dcd226de33b38d826fa0d3dc418034a5e21e27b8
Stijn Ruts <git@stijnruts.be>
2024-10-13 19:40:32 +0200

    Home manager

---
+++ configuration-home.nix
@@ -0,0 +1,25 @@
+{
+  config,
+  pkgs,
+  home-manager,
+  ...
+}:
+{
+  programs.home-manager.enable = true;
+  home.username = "stijn";
+  home.homeDirectory = "/home/stijn";
+
+  imports = [
+    ./home/shell.nix
+    ./home/git.nix
+    ./home/vim.nix
+  ];
+
+  # It is occasionally necessary for Home Manager to change configuration defaults
+  # in a way that is incompatible with stateful data. This could, for example,
+  # include switching the default data format or location of a file.
+  # The state version indicates which default settings are in effect and will therefore
+  # help avoid breaking program configurations. Switching to a higher state version
+  # typically requires performing some manual steps, such as data conversion or moving files.
+  home.stateVersion = "24.05";
+}
