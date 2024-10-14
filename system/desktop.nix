5c09f4931c9f4e348184d43745a2a6c84b0c39a4
Stijn Ruts <git@stijnruts.be>
2024-10-14 11:57:05 +0200

    XMonad

--- system/desktop.nix
+++ system/desktop.nix
@@ -4,5 +4,24 @@
   services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
+  services.xserver = {
+    windowManager = {
+      xmonad = {
+        enable = true;
+        enableContribAndExtras = true;
+        enableConfiguredRecompile = true;
+        # extraPackages = haskellPackages : [
+        #   haskellPackages.xyz
+        # ];
+      };
+    };
+  };
+
+  system.userActivationScripts.xmonadSymlink.text = ''
+    ln -sf $HOME/NixOS/system/xmonad $HOME/.config
+  '';
+
+  services.displayManager.defaultSession = "plasmax11";
+
   programs.firefox.enable = true;
 }
