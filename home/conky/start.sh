b024ee1e2083c6463f48ae71db3ebe05e74fb794
Stijn Ruts <git@stijnruts.be>
2025-08-15 08:00:59 +0200

    P520 conky

--- home/conky/start.sh
+++ home/conky/start.sh
@@ -4,9 +4,12 @@ CONFIGDIR=~/NixOS/home/conky
 
 case $(hostname) in
 
-  P520)
-    echo "TODO: P520"
-    # TODO: https://conky.cc/variables#nvidia
+  P520) # 2560 x 1440
+    conky --config=$CONFIGDIR/system.lua  -x  235 -y 200 &
+    conky --config=$CONFIGDIR/cpu.lua     -x  605 -y 200 &
+    conky --config=$CONFIGDIR/memory.lua  -x  975 -y 200 &
+    conky --config=$CONFIGDIR/disks.lua   -x 1345 -y 200 &
+    conky --config=$CONFIGDIR/network.lua -x 1715 -y 200
     ;;
 
   X201) # 1280 x 800
