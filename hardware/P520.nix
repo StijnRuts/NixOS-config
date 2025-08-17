ba86f6d1fbf7d79f87f8249212d2ffa55e98cd08
Stijn Ruts <git@stijnruts.be>
2025-08-17 20:29:34 +0200

    Don't wait 1m30 for tpm on boot

--- hardware/P520.nix
+++ hardware/P520.nix
@@ -27,6 +27,10 @@
   boot.kernelModules = [ "kvm-intel" ];
   boot.extraModulePackages = [ ];
 
+  boot.kernelParams = [
+    "systemd.mask=dev-tpmrm0.device" # don't wait 1m30 for tpm on boot
+  ];
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
