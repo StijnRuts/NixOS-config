1ff49e44f5a3f35520dcbe5d21dc29bdeb4a2aa0
Stijn Ruts <git@stijnruts.be>
2025-09-02 20:53:38 +0200

    Comment agenix

--- secrets/secrets.nix
+++ secrets/secrets.nix
@@ -2,6 +2,7 @@ let
   stijn = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIF98vuW/0StmEkZ5jXXvli5sD3fTcdn56M+9xhV1F5u/";
 in
 {
+  # \cat somefile | agenix -e somefile.age
   "hashed_password.age".publicKeys = [ stijn ];
   "rootCA_cert.age".publicKeys = [ stijn ];
   "rootCA_cert_key.age".publicKeys = [ stijn ];
