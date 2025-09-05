36dd371a9418372054118b75a3539a56e56c03c8
Stijn Ruts <git@stijnruts.be>
2025-09-06 01:03:04 +0200

    Fix shell init in distrobox

--- apps/utility/distrobox.nix
+++ apps/utility/distrobox.nix
@@ -8,6 +8,20 @@
 
   virtualisation.podman.enable = true;
 
+  environment.etc."distrobox/distrobox.conf".text =
+    let
+      volumes = paths: builtins.concatStringsSep " " (map (path: "${path}:${path}:ro") paths);
+    in
+    ''
+      container_additional_volumes="${
+        volumes [
+          "/nix/store"
+          "/etc/profiles/per-user"
+          "/etc/static/profiles/per-user"
+        ]
+      }"
+    '';
+
   persist.home = {
     directories = [
       ".cache/distrobox"
