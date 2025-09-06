1180291651249a95b7ed63dccfe7b31e0c740dae
Stijn Ruts <git@stijnruts.be>
2025-09-06 19:09:14 +0200

    Improve inputs of partials

--- partial/home/flake.nix
+++ partial/home/flake.nix
@@ -1,15 +1,10 @@
 {
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-25.05";
-    };
-    home-manager = {
-      url = "github:nix-community/home-manager/release-25.05";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
     config = {
       url = "path:../..";
     };
+    nixpkgs.follows = "config/nixpkgs";
+    home-manager.follows = "config/home-manager";
   };
   outputs =
     inputs:
--- partial/vim/flake.nix
+++ partial/vim/flake.nix
@@ -1,29 +1,21 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
-    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nvf = {
-      url = "github:NotAShelf/nvf";
-      inputs.nixpkgs.follows = "nixpkgs";
+    config = {
+      url = "path:../..";
     };
+    nixpkgs.follows = "config/nixpkgs";
+    nvf.follows = "config/nvf";
   };
   outputs = inputs: {
     packages.x86_64-linux = {
       default =
         let
-          pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
-          pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.x86_64-linux;
-          themeConfig = (import ../../theme/flake.nix).outputs {
-            inherit (inputs) nixpkgs;
-            catppuccin = null;
-          };
           vimConfig = (import ../../apps/cli/vim.nix) {
-            inherit pkgs-unstable;
-            inherit (themeConfig.args) theme;
+            inherit (inputs.config.args) pkgs-unstable theme;
           };
         in
         (inputs.nvf.lib.neovimConfiguration {
-          inherit pkgs;
+          pkgs = inputs.nixpkgs.legacyPackages.x86_64-linux;
           modules = [
             {
               config.vim = vimConfig.programs.nvf.settings.vim;
