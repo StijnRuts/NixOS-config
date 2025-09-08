1aceae570994c65588aa0914a0e238f165930c90
Stijn Ruts <git@stijnruts.be>
2025-09-08 11:56:28 +0200

    Disable baloo

--- KDE/desktop.nix
+++ KDE/desktop.nix
@@ -1,6 +1,12 @@
+{ pkgs, ... }:
 {
   services.desktopManager.plasma6.enable = true;
   programs.kdeconnect.enable = true;
+
+  environment.plasma6.excludePackages = with pkgs.kdePackages; [
+    baloo
+    baloo-widgets
+  ];
 
   persist.home = {
     directories = [
