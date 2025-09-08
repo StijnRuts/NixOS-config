9cc08fa04fc37b024378e96a82108df0fcce5bb2
Stijn Ruts <git@stijnruts.be>
2025-09-08 22:41:35 +0200

    Suppress output from WezTerm init

--- apps/gui/wezterm.nix
+++ apps/gui/wezterm.nix
@@ -20,13 +20,13 @@
       # Setup fancy environment, only on WezTerm
       if [ -n "$WEZTERM_EXECUTABLE" ]; then
         # Enable direnv
-        if command -v direnv; then
+        if command -v direnv >/dev/null 2>&1; then
           eval "$(direnv hook zsh)"
         fi
         # If not in nix-shell or devenv
         if [ -z "$IN_NIX_SHELL" ] && [ -z "$DEVENV_DOTFILE" ]; then
           # Launch zellij
-          if command -v zellij; then
+          if command -v zellij >/dev/null 2>&1; then
             # If not already in Zellij
             if [[ -z "$ZELLIJ" ]]; then
               zellij attach -c default
