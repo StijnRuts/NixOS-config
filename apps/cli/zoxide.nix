62639bfe4e08b7e1286c4558a196cd7f3241403e
Stijn Ruts <git@stijnruts.be>
2025-09-19 13:58:16 +0200

    Zoxide fallback script

--- apps/cli/zoxide.nix
+++ apps/cli/zoxide.nix
@@ -1,10 +1,47 @@
+{ pkgs, ... }:
 {
-  programs.zoxide = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    enableNushellIntegration = true;
+  programs = {
+    zoxide = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableNushellIntegration = true;
+      options = [ "--no-cmd" ]; # Replace the `z` shortcut with a custom script
+    };
+    bash.initExtra = ''
+      function z {
+        local tmpfile=$(mktemp)
+        zoxide_fallback "$@" | tee "$tmpfile"
+        local dir=$(tail -n 1 "$tmpfile")
+        rm "$tmpfile"
+        [ -d "$dir" ] && cd "$dir"
+      }
+    '';
+    zsh.initContent = ''
+      function z {
+        local tmpfile=$(mktemp)
+        zoxide_fallback "$@" | tee "$tmpfile"
+        local dir=$(tail -n 1 "$tmpfile")
+        rm "$tmpfile"
+        [ -d "$dir" ] && cd "$dir"
+      }
+    '';
+    nushell.extraConfig = ''
+      def z [...args] {
+        let tmpfile = mktemp
+        zoxide_fallback ...$args | tee { save $tmpfile }
+        let dir = (open $tmpfile | last)
+        rm $tmpfile
+        if ($dir | path exists) { cd $dir }
+      }
+    '';
   };
+
+  home.packages = [
+    pkgs.fd
+    pkgs.fzf
+    (pkgs.writers.writeBashBin "zoxide_fallback" (builtins.readFile ./zoxide_fallback.sh))
+  ];
 
   persist.home = {
     directories = [
---
+++ apps/cli/zoxide_fallback.sh
@@ -0,0 +1,76 @@
+#!/usr/bin/env bash
+
+function search {
+  if [ -z "$query" ]; then
+    echo "  No search string given"
+    exit 1
+  fi
+
+  zoxide_search
+  navigate
+
+  find_search
+  navigate
+
+  echo "  No matching directories found"
+  exit 1
+}
+
+function zoxide_search {
+  matches=$(zoxide query --list "$query")
+}
+
+function find_search {
+  echo "󰍉 Searching filesystem"
+  if command -v fd &>/dev/null; then
+    matches=$(fd --type d "$query" ~ 2>/dev/null)
+  else
+    matches=$(find ~ -type d -iname "*$query*" 2>/dev/null)
+  fi
+}
+
+function navigate {
+  readarray -t list < <(echo "$matches")
+  if [ ${#list[@]} -eq 0 ] || [ -z "${list[0]}" ]; then
+    return
+  elif [ ${#list[@]} -eq 1 ]; then
+    echo "  Found:"
+    echo "${list[0]}"
+    exit 0
+  else
+    echo "  Multiple matches found"
+    select_from_list
+    echo "  Selected:"
+    echo "$selected"
+    exit 0
+  fi
+}
+
+function select_from_list {
+  if command -v fzf &>/dev/null; then
+    select_fzf
+  else
+    select_builtin
+  fi
+}
+
+function select_fzf {
+  selected=$(
+    echo "$matches" | fzf \
+      --prompt="  Select directory > " \
+      --height=6 --style=minimal \
+      --layout=reverse --info=hidden
+  )
+}
+
+function select_builtin {
+  PS3="  Select a directory: "
+  select selected in "${list[@]}"; do
+    if [ -n "$selected" ]; then
+      return
+    fi
+  done
+}
+
+query="$*"
+search
