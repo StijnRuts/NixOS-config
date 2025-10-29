b378190559e53afc0c1df80d7e6620a2a6760e5f
Stijn Ruts <git@stijnruts.be>
2025-10-29 20:30:31 +0100

    Zellij copy text

--- apps/cli/zellij.nix
+++ apps/cli/zellij.nix
@@ -12,11 +12,13 @@
       session_serialization = true;
       pane_viewport_serialization = true;
       scrollback_lines_to_serialize = 500;
+      copy_on_select = false;
     };
   };
 
   catppuccin.zellij.enable = true;
 
+  # https://github.com/zellij-org/zellij/blob/main/zellij-utils/assets/config/default.kdl
   # Change ctrl keybinds to alt
   xdg.configFile."zellij/config.kdl" = {
     text = builtins.readFile ./zellij.kdl;
