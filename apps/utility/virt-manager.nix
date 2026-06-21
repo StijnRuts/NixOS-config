3420b0bf5f8e41c18442cfc3313e323d4d1c27af
Stijn Ruts <git@stijnruts.be>
2026-06-21 12:19:11 +0200

    Fix virt-manager networking

--- apps/utility/virt-manager.nix
+++ apps/utility/virt-manager.nix
@@ -1,8 +1,17 @@
-{ config, lib, ... }:
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
 {
   config = lib.mkIf config.apps.utilities.enable {
     programs.virt-manager.enable = true;
     virtualisation.libvirtd.enable = true;
+
+    networking.firewall.trustedInterfaces = [ "virbr0" ];
+    users.users.stijn.extraGroups = [ "libvirtd" ];
+    environment.systemPackages = with pkgs; [ dnsmasq ];
 
     persist.system = {
       directories = [
