a98e2392ec41ad64c75ff732968f69eb3d4105ce
Stijn Ruts <git@stijnruts.be>
2026-06-25 19:09:09 +0200

    Extra droid options

--- modules/droid/nix-on-droid.nix
+++ modules/droid/nix-on-droid.nix
@@ -29,6 +29,15 @@
 
   environment.etcBackupExtension = ".bak";
 
+  android-integration = {
+    am.enable = true;
+    termux-open.enable = true;
+    termux-open-url.enable = true;
+    termux-reload-settings.enable = true;
+    termux-setup-storage.enable = true;
+    xdg-open.enable = true;
+  };
+
   system.stateVersion = "24.05";
 
   # Configure home-manager
@@ -37,4 +46,30 @@
     backupFileExtension = "hm-bak";
     useGlobalPkgs = true;
   };
+
+  user.shell = "${pkgs.zsh}/bin/zsh";
+
+  terminal.colors = {
+    background = "#1e1e2e";
+    foreground = "#cdd6f4";
+    cursor = "#f5e0dc";
+    color0 = "#45475a";
+    color1 = "#f38ba8";
+    color2 = "#a6e3a1";
+    color3 = "#f9e2af";
+    color4 = "#89b4fa";
+    color5 = "#f5c2e7";
+    color6 = "#94e2d5";
+    color7 = "#a6adc8";
+    color8 = "#585b70";
+    color9 = "#f37799";
+    color10 = "#89d88b";
+    color11 = "#ebd391";
+    color12 = "#74a8fc";
+    color13 = "#f2aede";
+    color14 = "#6bd7ca";
+    color15 = "#bac2de";
+  };
+
+  terminal.font = "${pkgs.nerd-fonts.ubuntu}/share/fonts/truetype/NerdFonts/Ubuntu/UbuntuNerdFontPropo-Regular.ttf";
 }
